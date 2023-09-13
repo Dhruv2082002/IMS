a1e1501e671d6a775c3e984d83475d7baeeb5e14
Dhruv Shah <dushah2002@gmail.com>
2023-09-14 02:05:46 +0530

    Add project files.

---
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/Migrations/20230904073132_MyMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMS.Plugins.EFCoreSqlServer.Migrations
+{
+    public partial class MyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductName",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Inventories_InventoryName",
+                table: "Inventories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductName",
+                table: "Products",
+                column: "ProductName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_InventoryName",
+                table: "Inventories",
+                column: "InventoryName",
+                unique: true);
+        }
+    }
+}
